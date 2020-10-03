f2a074382023eace9e4be0cb4c55bf86abe1a9b4
Cauê Santos <nokylevs@gmail.com>
2020-10-03 20:57:35 -0300

    Incrementando valores no placar de acordo com o ganhador

--- pedra-papel-tesoura/js/script.js
+++ pedra-papel-tesoura/js/script.js
@@ -11,6 +11,11 @@ document.getElementById('tesoura').addEventListener('click', function(){
 var padrao = document.getElementById('padrao');
 var alertaResultado = document.getElementById('alertaResultado');
 var spanResultado = document.getElementById('resultado');
+var placarPC = document.getElementById('placar-pc');
+var placarUsuario = document.getElementById('placar-usuario');
+
+var usuario = 0;
+var pc = 0;
 
 function simulaJogadaMaquina(jogadaUsuario){
     let jogadas = ['pedra', 'papel', 'tesoura'];
@@ -18,11 +23,11 @@ function simulaJogadaMaquina(jogadaUsuario){
     let jogadaFinal = jogadas[posicao];
 
     if(jogadaFinal == 'pedra'){
-        padrao.src= "../img/pedra.png"; 
+        padrao.src= "./img/pedra.png"; 
     } else if (jogadaFinal == 'papel'){
-        padrao.src= "../img/papel.png"; 
+        padrao.src= "./img/papel.png"; 
     } else if (jogadaFinal == 'tesoura'){
-        padrao.src= "../img/tesoura.png"; 
+        padrao.src= "./img/tesoura.png"; 
     }
     
     calculaResultado(jogadaUsuario, jogadas[posicao]);
@@ -34,12 +39,14 @@ function calculaResultado(usuario, maquina){
     usuario == "tesoura" && maquina == "papel" ||
     usuario == "pedra" && maquina == "tesoura"){
         notificacaoVencedor();
+        alteraPlacar("usuario");
     } 
     //máquina vencedora
     else if(maquina == "papel" && usuario == "pedra" ||
     maquina == "tesoura" && usuario == "papel" ||
     maquina == "pedra" && usuario == "tesoura"){
         notificacaoDerrota();
+        alteraPlacar("pc");
     }
     //empate
     else{
@@ -68,4 +75,14 @@ function notificacaoEmpate(){
         alertaResultado.classList.remove('d-none');
       }
       spanResultado.innerText = 'Empate.';  
+}
+
+function alteraPlacar(vencedor){
+    if(vencedor === "pc"){
+        pc++;
+        placarPC.innerText = "PC: " + pc;
+    } else {
+        usuario++;
+        placarUsuario.innerText = "VOCÊ: " + usuario;
+    }
 }
